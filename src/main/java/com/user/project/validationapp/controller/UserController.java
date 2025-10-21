5a27df1dd9c6bb5bd8d5c0f38be99db8ad17e55b
ivan <lorzproducciones@gmail.com>
2025-10-21 12:17:12 -0400

    CRUD terminado

--- src/main/java/com/user/project/validationapp/controller/UserController.java
+++ src/main/java/com/user/project/validationapp/controller/UserController.java
@@ -2,18 +2,17 @@ package com.user.project.validationapp.controller;
 
 import com.user.project.validationapp.model.User;
 import com.user.project.validationapp.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    UserService userService;
+    private final UserService userService;
 
     @GetMapping("/users")
     public List<User> allUserAsList(){
@@ -23,5 +22,16 @@ public class UserController {
     @PostMapping("/create")
     public void createUserC(@RequestBody User user){
         userService.createUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public void updateUserC(@PathVariable Long id, @RequestBody User user){
+
+        userService.updateUser(id,user);
+    }
+
+    @DeleteMapping("/delete{id}")
+    public void deleteUserC(@PathVariable Long id){
+        userService.deleteUser(id);
     }
 }
